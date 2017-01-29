2b123245bf588b9bcebb0897da443101d0362b9c
mh-izadi <mhizadi92@gmail.com>
2017-01-29 15:17:47 +0330

    check transaction forms validation

--- TransactionManagement/forms.py
+++ TransactionManagement/forms.py
@@ -13,6 +13,25 @@ class WithdrawForm(forms.Form):
     bank_account_id = forms.IntegerField(min_value=0, error_messages=field_errors)
     amount = forms.IntegerField(min_value=0, error_messages=field_errors)
 
+    def clean_bank_account_id(self):
+        bank_account_id = self.cleaned_data.get('bank_account_id')
+        try:
+            bankAccount=BankAccount.objects.get(account_id=bank_account_id)
+            if bankAccount.customer.activated == False:
+                raise forms.ValidationError('این حساب مسدود است!')
+        except BankAccount.DoesNotExist:
+            raise forms.ValidationError('حسابی با این شماره یافت نشد!')
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        bank_account_id = self.cleaned_data.get('bank_account_id')
+        try:
+            bankAccount=BankAccount.objects.get(account_id=bank_account_id)
+            if amount>bankAccount.amount:
+                raise forms.ValidationError('موجودی کافی نیست!')
+        except:
+            raise forms.ValidationError('خطایی در مقدار وجود دارد!')
+
     def save(self):
         bank_account_id = self.cleaned_data.get('bank_account_id')
         amount = self.cleaned_data.get('amount')
@@ -34,11 +53,38 @@ class DepositToOtherForm(forms.Form):
     destination_bank_account_id = forms.IntegerField(min_value=0, error_messages=field_errors)
     amount = forms.IntegerField(min_value=0, error_messages=field_errors)
 
+    def clean_source_bank_account_id(self):
+        source_bank_account_id = self.cleaned_data.get('source_bank_account_id')
+        try:
+            bankAccount=BankAccount.objects.get(account_id=source_bank_account_id)
+            if bankAccount.customer.activated == False:
+                raise forms.ValidationError('این حساب مسدود است!')
+        except BankAccount.DoesNotExist:
+            raise forms.ValidationError('حسابی با این شماره یافت نشد!')
+
+    def clean_destination_bank_account_id(self):
+        destination_bank_account_id = self.cleaned_data.get('destination_bank_account_id')
+        try:
+            bankAccount=BankAccount.objects.get(account_id=destination_bank_account_id)
+            if bankAccount.customer.activated == False:
+                raise forms.ValidationError('این حساب مسدود است!')
+        except BankAccount.DoesNotExist:
+            raise forms.ValidationError('حسابی با این شماره یافت نشد!')
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        source_bank_account_id = self.cleaned_data.get('source_bank_account_id')
+        try:
+            bankAccount=BankAccount.objects.get(account_id=source_bank_account_id)
+            if amount>bankAccount.amount:
+                raise forms.ValidationError('موجودی کافی نیست!')
+        except:
+            raise forms.ValidationError('خطایی در مقدار وجود دارد!')
+
     def save(self):
         source_bank_account_id = self.cleaned_data.get('source_bank_account_id')
         destination_bank_account_id = self.cleaned_data.get('destination_bank_account_id')
         amount = self.cleaned_data.get('amount')
-
         while True:
             try:
                 source_bank_account = BankAccount.objects.get(account_id=source_bank_account_id)
@@ -57,6 +103,15 @@ class DepositToOtherForm(forms.Form):
 class DepositForm(forms.Form):
     bank_account_id = forms.IntegerField(min_value=0, error_messages=field_errors)
     amount = forms.IntegerField(min_value=0, error_messages=field_errors)
+
+    def clean_bank_account_id(self):
+        bank_account_id = self.cleaned_data.get('bank_account_id')
+        try:
+            bankAccount=BankAccount.objects.get(account_id=bank_account_id)
+            if bankAccount.customer.activated == False:
+                raise forms.ValidationError('این حساب مسدود است!')
+        except BankAccount.DoesNotExist:
+            raise forms.ValidationError('حسابی با این شماره یافت نشد!')
 
     def save(self):
         bank_account_id = self.cleaned_data.get('bank_account_id')
