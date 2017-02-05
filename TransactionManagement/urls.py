83cbccbd33d8b67f66d0b6eb25cbb4b676d62427
mh-izadi <mhizadi92@gmail.com>
2017-02-05 17:05:05 +0330

    add cash bill payment page

--- TransactionManagement/urls.py
+++ TransactionManagement/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^DepositBankAccount/', view=views.deposit_to_bank_account, name='DepositBankAccount'),
     url(r'^DepositToOtherBankAccount/', view=views.deposit_to_other_bank_account, name='DepositToOtherBankAccount'),
     url(r'^aReport/', view=views.accountant_report, name='AccountantReport'),
-    url(r'^BillPayement/', view=views.bill_payment, name='BillPayement')
+    url(r'^BillPayment/', view=views.bill_payment, name='BillPayment'),
+    url(r'^CashBillPayment/', view=views.cash_bill_payment, name='CashBillPayment')
 ]
